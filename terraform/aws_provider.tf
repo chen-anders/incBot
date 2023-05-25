795f6d0607ed386046a1250d51aeedff73eb969f
Anders Chen <anders@wistia.com>
2023-05-25 14:12:59 -0400

    Add wistia-specific config

--- terraform/aws_provider.tf
+++ terraform/aws_provider.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket         = "wistia-deploy"
+    key            = "terraform-states/slack-incbot.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
--- terraform/inc-bot.tf
+++ terraform/inc-bot.tf
@@ -5,5 +5,5 @@ module "inc-bot" {
   secrets_key = "app/inc-bot/secrets"
 
   incident_broadcast_channel_id = "C23SFSW1L"
-  pagerduty_email               = "my-test-email@testing.com"
+  pagerduty_email               = "engineering-accounts@wistia.com"
 }
