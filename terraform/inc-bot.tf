3a754d0fa1d0268b2bc32e1e6a4fc5a5383b349d
Anders Chen <anders@wistia.com>
2023-05-26 00:30:14 -0400

    Recreate testing inc-bot for test workspace

--- terraform/inc-bot.tf
+++ terraform/inc-bot.tf
@@ -8,3 +8,14 @@ module "inc-bot" {
   pagerduty_email               = "engineering-accounts@wistia.com"
   enable_lambda_warmer          = true
 }
+
+module "inc-bot-testing" {
+  source = "./inc-bot"
+
+  name        = "inc-bot-testing"
+  secrets_key = "app/incident-helper-testing-bot/secrets"
+
+  incident_broadcast_channel_id = "C058J95SNVC"
+  pagerduty_email               = "engineering-accounts@wistia.com"
+  enable_lambda_warmer          = false
+}
